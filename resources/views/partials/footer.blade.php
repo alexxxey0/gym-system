698f93503418e98c8da8f2c07509ff698f25c414
alexxxey <aleksejs.gorlovics@gmail.com>
2024-10-10 11:30:13 +0300

    changed text language to Latvian

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/partials/footer.blade.php -->
 <footer class="bg-gray-900 text-white p-4 mt-8 flex flex-row justify-between">
     <div class="container mx-auto text-center">
-        <p>&copy; {{ date('Y') }} FitLife. All rights reserved.</p>
+        <p>&copy; {{ date('Y') }} FitLife. Visas tiesības aizsargātas.</p>
     </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,13 +12,13 @@
             width: 100%;
         }
     </style>
-    <h1 class='text-[#007BFF] text-center text-6xl py-8'>Welcome to FitLife!</h1>
+    <h1 class='text-[#007BFF] text-center text-6xl py-8'>Laipni lūdzam FitLife!</h1>
     <form class='flex flex-col w-10/12 md:w-1/3 mx-auto shadow-md rounded-md p-4 border-2 border-[#C0C0C0]' action="">
-        <label for="personal-id">Personal ID</label>
+        <label for="personal-id">Personas kods</label>
         <input class='border-2 border-gray-300 rounded-sm' type="text" name='personal-id'>
-        <label for="password">Password</label>
+        <label for="password">Parole</label>
         <input class='border-2 border-gray-300 rounded-sm' type="password" name='password'>
-        <button class='mt-4 bg-[#007BFF] active:bg-[#0056b3] text-white py-2 w-1/2 md:w-1/4 mx-auto rounded-md' type="submit">Log In</button>
+        <button class='mt-4 bg-[#007BFF] active:bg-[#0056b3] text-white py-2 w-1/2 md:w-1/4 mx-auto rounded-md' type="submit">Pieslēgties</button>
     </form>
     <h2 class='text-gray-300 text-center'></h2>
 @endsection
