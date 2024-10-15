8f1435a345911b7885f66f11f06af71351270fbd
alexxxey <aleksejs.gorlovics@gmail.com>
2024-10-15 15:26:19 +0300

    added website icon

--- resources/views/layouts/footer_only.blade.php
+++ resources/views/layouts/footer_only.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My Laravel App')</title>
+    <link rel="shortcut icon" href="{{ asset('images/fitlife_icon.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
 </head>
 <body>
